d323b033eef3a0db2e8e02bfe70e1705792b96b3
Frederic Junod <frederic.junod@camptocamp.com>
2017-02-15 16:01:29 +0100

    Inital commit

---
+++ www/js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('deviceready', function() {
+
+  var map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    target: 'map',
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
+
+}, false);
